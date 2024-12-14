9690eaceb9da4a4f83f8e640c447153707fa39d5
romitpatel <patelromit6952@gmail.com>
2024-12-14 09:23:12 +0530

    first commit

---
+++ firstapp/lib/practice.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  // print("romit patel");
+  // stdout.write("enter your name: ");
+  // var name = stdin.readLineSync();
+  // print("welcome,$name");
+  //var romit = new human();
+
+  // declaration of variable
+  // int = a;
+
+}
+
+class  human{
+  human();
+}
